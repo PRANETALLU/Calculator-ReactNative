3fb8aa3b7a35722de6683b31b52b3b776a9d1bde
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2024-03-25 01:55:31 -0400

    individual buttons setup

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Buttons from './components/Buttons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>fdsfs</Text>
-      <Buttons />
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <Buttons value="7" color="#222222" />
+        <Buttons value="8" color="#222222" />
+        <Buttons value="9" color="#222222" />
+        <Buttons value="x" color="#FFAC1C" />
+      </View>
+
+      <View style={styles.row}>
+        <Buttons value="4" color="#222222" />
+        <Buttons value="5" color="#222222" />
+        <Buttons value="6" color="#222222" />
+        <Buttons value="-" color="#FFAC1C" />
+      </View>
+
+      <View style={styles.row}>
+        <Buttons value="1" color="#222222" />
+        <Buttons value="2" color="#222222" />
+        <Buttons value="3" color="#222222" />
+        <Buttons value="+" color="#FFAC1C" />
+      </View>
+
+      <View style={styles.row}>
+        <Buttons value="0" color="#222222" />
+        <Buttons value="." color="#222222" />
+        <Buttons value="=" color="#FFAC1C" />
+      </View>
     </View>
   );
 }
@@ -15,11 +38,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000000',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-  button: {
-    backgroundColor: 'white'
-  }
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
--- AwesomeProject/components/Buttons.js
+++ AwesomeProject/components/Buttons.js
@@ -1,7 +1,24 @@
-import { Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function Buttons() {
-    return(
-        <Button title="1" color="blue" />
-    ); 
+export default function Buttons(props) {
+    return (
+        <TouchableOpacity
+            style={{
+                backgroundColor: props.color,
+                
+                width: 30,     
+                height: 30,
+                
+                borderRadius: 50, 
+                
+                justifyContent: "center",
+                alignItems: "center",
+
+                marginRight: 10,
+                marginBottom: 10
+            }}
+        >
+            <Text style={{color: "white"}}>{props.value}</Text>
+        </TouchableOpacity>
+    );
 }
