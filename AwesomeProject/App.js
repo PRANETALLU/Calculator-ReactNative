13d07a30d81d5312db604c9dbf8752df69d91a99
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2024-04-01 01:45:25 -0400

    Additional Changes

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,47 +4,48 @@ import Buttons from './components/Buttons';
 import { useState } from 'react';
 
 export default function App() {
-  const { value1, setValue } = useState("0");
+  const [ value1, setValue ] = useState("");
   
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <Text value={value1} style={{color: "white"}}>
+      <View style={styles.rowDisplay}>
+        <Text value={value1} style={{ color: 'white', textAlign: "right" }}>
+          {value1}
         </Text>
       </View>
 
       <View style={styles.row}>
-        <Buttons value="C" color="#7D7F7C" />
-        <Buttons value="+/-" color="#7D7F7C" />
-        <Buttons value="%" color="#7D7F7C" />
-        <Buttons value="/" color="#FFAC1C" />
+        <Buttons value="C" color="#7D7F7C" setValue={setValue} />
+        <Buttons value="+/-" color="#7D7F7C" setValue={setValue} />
+        <Buttons value="%" color="#7D7F7C" setValue={setValue} />
+        <Buttons value="/" color="#FFAC1C" setValue={setValue} />
       </View>
 
       <View style={styles.row}>
         <Buttons value="7" color="#222222" setValue={setValue} />
         <Buttons value="8" color="#222222" setValue={setValue} />
         <Buttons value="9" color="#222222" setValue={setValue} />
-        <Buttons value="x" color="#FFAC1C" />
+        <Buttons value="x" color="#FFAC1C" setValue={setValue} />
       </View>
 
       <View style={styles.row}>
-        <Buttons value="4" color="#222222" />
-        <Buttons value="5" color="#222222" />
-        <Buttons value="6" color="#222222" />
-        <Buttons value="-" color="#FFAC1C" />
+        <Buttons value="4" color="#222222" setValue={setValue} />
+        <Buttons value="5" color="#222222" setValue={setValue} />
+        <Buttons value="6" color="#222222" setValue={setValue} />
+        <Buttons value="-" color="#FFAC1C" setValue={setValue} />
       </View>
 
       <View style={styles.row}>
-        <Buttons value="1" color="#222222" />
-        <Buttons value="2" color="#222222" />
-        <Buttons value="3" color="#222222" />
-        <Buttons value="+" color="#FFAC1C" />
+        <Buttons value="1" color="#222222" setValue={setValue} />
+        <Buttons value="2" color="#222222" setValue={setValue} />
+        <Buttons value="3" color="#222222" setValue={setValue} />
+        <Buttons value="+" color="#FFAC1C" setValue={setValue} />
       </View>
 
       <View style={styles.row}>
-        <Buttons value="0" color="#222222" />
-        <Buttons value="." color="#222222" />
-        <Buttons value="=" color="#FFAC1C" />
+        <Buttons value="0" color="#222222" setValue={setValue} />
+        <Buttons value="." color="#222222" setValue={setValue} />
+        <Buttons value="=" color="#FFAC1C" setValue={setValue} />
       </View>
     </View>
   );
@@ -55,10 +56,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000000',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    //backgroundColor: 'blue'
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+  rowDisplay: {
+    flexDirection: 'row',
+    justifyContent:"flex-end",
+    width: '20%',
+    marginBottom: 10,
+    //backgroundColor: "red"
+  }
 });
--- AwesomeProject/components/Buttons.js
+++ AwesomeProject/components/Buttons.js
@@ -1,8 +1,17 @@
 import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function Buttons(props) {
+    const changeVal = () => {
+        if(props.value == "C") {
+            props.setValue("");
+        }
+        else {
+            props.setValue(prevValue => prevValue + props.value); 
+        }
+    }
     return (
-        <TouchableOpacity
+        <>
+        {props.value != "0" && (<TouchableOpacity
             style={{
                 backgroundColor: props.color,
                 
@@ -17,9 +26,30 @@ export default function Buttons(props) {
                 marginRight: 10,
                 marginBottom: 10
             }}
-            onPress={() => props.setValue(props.value)}
+            onPress={changeVal}
         >
             <Text style={{color: "white"}}>{props.value}</Text>
-        </TouchableOpacity>
+        </TouchableOpacity>)}
+         
+        {props.value == "0" && (<TouchableOpacity
+            style={{
+                backgroundColor: props.color,
+                
+                width: 70,     
+                height: 30,
+                
+                borderRadius: 50, 
+                
+                justifyContent: "center",
+                alignItems: "center",
+
+                marginRight: 10,
+                marginBottom: 10
+            }}
+            onPress={changeVal}
+        >
+            <Text style={{color: "white"}}>{props.value}</Text>
+        </TouchableOpacity>)}
+        </>
     );
 }
